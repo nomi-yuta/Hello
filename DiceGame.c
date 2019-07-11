2f7d3f6f81c1a43b17fabfdf33d761af263b0636
s1260141 <s1260141@lsrdc6>
2019-07-11 18:50:37 +0900

    make 2

---
+++ DiceGame.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+
+  int a ,b,c;
+  srand(time(NULL));
+  a = rand()%6+1;
+  b = rand()%6+1;
+  c=a+b;
+  printf("Die 1: %d\n",a);
+   printf("Die 2: %d\n",b);
+    printf("Total value: %d\n",c);
+
+    return 0;
+}
